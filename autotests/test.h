64198bf197c7c5e66d83c2b229e1c831ffb514e5
Ralf Habacker <ralf.habacker@freenet.de>
2020-11-11 23:15:43 +0100

    Add fake style for QApplication
    results into crash in native launch mode
    
    QDEBUG : AlkOnlineQuoteTest::testLaunch() alkonlinequotestest(6255)/kio (KIOJob) KIO::SlaveInterface::dispatch: needs a msg box
    QFATAL : AlkOnlineQuoteTest::testLaunch() QWidget: Cannot create a QWidget when no GUI is being used

--- autotests/test.h
+++ autotests/test.h
@@ -1,13 +1,34 @@
 #ifndef TEST_H
 #define TEST_H
 
+#include <QPalette>
+#include <QStyle>
 #include <QtTest/QtTest>
 
+class MyStyle : public QStyle
+{
+public:
+    void drawPrimitive(PrimitiveElement, const QStyleOption*, QPainter*, const QWidget*) const {}
+    void drawControl(ControlElement, const QStyleOption*, QPainter*, const QWidget*) const {}
+    QRect subElementRect(SubElement, const QStyleOption*, const QWidget*) const { return QRect(); }
+    void drawComplexControl(ComplexControl, const QStyleOptionComplex*, QPainter*, const QWidget*) const {}
+    SubControl hitTestComplexControl(ComplexControl, const QStyleOptionComplex*, const QPoint&, const QWidget*) const { return SubControl(); }
+    QRect subControlRect(ComplexControl, const QStyleOptionComplex*, SubControl, const QWidget*) const { return QRect(); }
+    int pixelMetric(PixelMetric, const QStyleOption*, const QWidget*) const { return 0; }
+    QSize sizeFromContents(ContentsType, const QStyleOption*, const QSize&, const QWidget*) const { return QSize(); }
+    int styleHint(StyleHint, const QStyleOption*, const QWidget*, QStyleHintReturn*) const { return 0; }
+    QPixmap standardPixmap(StandardPixmap, const QStyleOption*, const QWidget*) const { return QPixmap(); }
+    QPixmap generatedIconPixmap(QIcon::Mode, const QPixmap&, const QStyleOption*) const { return QPixmap(); }
+};
 #if QT_VERSION < QT_VERSION_CHECK(5,0,0)
 #define QTEST_GUILESS_MAIN(TestObject) \
 int main(int argc, char *argv[]) \
 { \
-    QCoreApplication app(argc, argv); \
+    MyStyle style; \
+    QPalette palette; \
+    QApplication::setPalette(palette, "nongui"); \
+    QApplication::setStyle(&style); \
+    QApplication app(argc, argv, QApplication::Tty); \
     TestObject tc; \
     return QTest::qExec(&tc, argc, argv); \
 }
