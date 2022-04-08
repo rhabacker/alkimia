702d7a77de1ef931f2f77f6b0748cebc45718d7f
Ralf Habacker <ralf.habacker@freenet.de>
2022-04-08 17:08:28 +0200

    Initial support for documentation

---
+++ doc/entities.ent.cmake
@@ -0,0 +1,9 @@
+<!ENTITY version "@PROJECT_VERSION@">
+<!ENTITY date "@BUILD_DATE@">
+<!ENTITY target_suffix "@TARGET_SUFFIX@">
+<!ENTITY onlinequoteseditor "<application>Online Quotes Editor</application>">
+<!ENTITY package "office">
+<!ENTITY % addindex "IGNORE">
+<!ENTITY % English "INCLUDE"> <!-- ONLY If you are writing non-English
+                                   original documentation, change
+                                   the language here -->
