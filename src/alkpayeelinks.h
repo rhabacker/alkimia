ec0107fa39b89b82666a47dda94f86ebd6c07980
Ralf Habacker <ralf.habacker@freenet.de>
2022-10-04 22:27:30 +0200

    Add implementation for predefined payee link sources
    TODO: add support to check updates

--- src/alkpayeelinks.h
+++ src/alkpayeelinks.h
@@ -3,7 +3,83 @@
 
 #include <alkimia/alk_export.h>
 
+#include <QDate>
+#include <QUrl>
 #include <QObject>
+
+class AlkPayeeLinkConfig
+{
+public:
+    QDate timeStamp;
+    QString key;
+    QString name;
+    QString idPattern;
+    QString urlTemplate;
+    QString notes;
+
+    AlkPayeeLinkConfig() {}
+    AlkPayeeLinkConfig(const QString &data, int version)
+    {
+        QStringList cols = data.split(';');
+        if (cols.size() == 6) {
+            timeStamp = QDate::fromString(cols[0]);
+            key = cols[1];
+            name = cols[2];
+            idPattern = cols[3];
+            urlTemplate= cols[4];
+            notes = cols[5];
+        }
+    }
+
+    static bool match(const QString &data)
+    {
+        return data.count(';') == 5;
+    }
+
+    bool valid()
+    {
+        return !key.isEmpty() && !name.isEmpty() && !idPattern.isEmpty() && !urlTemplate.isEmpty();
+    }
+
+    QStringList matchingLinks(const QString& text) const
+    {
+      QStringList result;
+      if (idPattern.isEmpty() || urlTemplate.isEmpty())
+        return result;
+      QRegExp rx(idPattern.contains("(") ? QString("%1").arg(idPattern) : QString("(%1)").arg(idPattern));
+      if (rx.indexIn(text) == -1)
+        return result;
+      result = rx.capturedTexts();
+      return result;
+    }
+
+    QUrl payeeLink(const QString& text) const
+    {
+        QStringList matches = matchingLinks(text);
+        QString result;
+        if (matches.size() == 2) {
+            return QUrl(urlTemplate.arg(matches[1]));
+        } else if (matches.size() == 3) {
+            return QUrl(urlTemplate.arg(matches[1], matches[2]));
+        } else {
+            return QUrl();
+        }
+    }
+
+    QString decorateLink(const QString& text) const
+    {
+      QStringList matches = matchingLinks(text);
+      if (matches.size() == 0)
+        return text;
+      QRegExp rx(QString("(%1)").arg(matches[0]));
+
+      QString result = text;
+      result.replace(rx, "<u>\\1</u>");
+      return result;
+    }
+};
+
+typedef QList<AlkPayeeLinkConfig> AlkPayeeLinkConfigList;
 
 class ALK_EXPORT AlkPayeeLinks : public QObject
 {
@@ -13,6 +89,10 @@ public:
     explicit AlkPayeeLinks(QObject *parent = nullptr);
     ~AlkPayeeLinks();
 
+    AlkPayeeLinkConfigList &payeeLinks();
+    AlkPayeeLinkConfig &payeeLink(int i) const;
+    const AlkPayeeLinkConfig &payeeLink(const QString &key) const;
+    static AlkPayeeLinks &instance();
 private:
     class Private;
     Private *const d;
--- src/alkpayeelinkswidget.cpp
+++ src/alkpayeelinkswidget.cpp
@@ -1,14 +1,56 @@
 #include "alkpayeelinkswidget.h"
-#include "ui_alkpayeelinkswidget.h"
+#include "ui_alkpayeelinkswidgetimpl.h"
 
-class AlkPayeeLinksWidget::Private : public QWidget, public Ui::AlkPayeeLinksWidget
+#include "alkpayeelinks.h"
+
+class AlkPayeeLinksWidget::Private : public QWidget, public Ui::AlkPayeeLinksWidgetImpl
 {
+    Q_OBJECT
 public:
-    Private(QWidget *parent)
-    {
-        setupUi(parent);
-    }
+    Private(QWidget *parent);
+
+public slots:
+    void slotMatchingCheck(const QString &text);
 };
+
+AlkPayeeLinksWidget::Private::Private(QWidget *parent)
+{
+    setupUi(parent);
+    m_payeeLinkSource->addItem(i18n("Disabled"));
+    foreach(const AlkPayeeLinkConfig &config, AlkPayeeLinks::instance().payeeLinks()) {
+        m_payeeLinkSource->addItem(config.name, QVariant(config.key));
+    }
+    m_customMatchSettings->setEnabled(false);
+    connect(m_useCustomSource, SIGNAL(clicked(bool)), m_customMatchSettings, SLOT(setEnabled(bool)));
+    connect(m_useCustomSource, SIGNAL(clicked(bool)), m_payeeLinkSource, SLOT(setDisabled(bool)));
+    connect(m_identifierCheckEdit, SIGNAL(textChanged(QString)), this, SLOT(slotMatchingCheck(QString)));
+}
+
+void AlkPayeeLinksWidget::Private::slotMatchingCheck(const QString &text)
+{
+    m_capturedResult->setText("");
+    m_resultingURL->setText("");
+    AlkPayeeLinkConfig config;
+    if (m_useCustomSource->checkState() == Qt::Checked) {
+        if (m_idPatternEdit->text().isEmpty() || m_idPatternEdit->text().isEmpty())
+            return;
+        config.idPattern = m_idPatternEdit->text();
+        config.urlTemplate = m_urlTemplateEdit->text();
+    } else {
+        QString key = m_payeeLinkSource->currentData().toString();
+        if (key.isEmpty())
+            return;
+        config = AlkPayeeLinks::instance().payeeLink(key);
+    }
+    QStringList matches = config.matchingLinks(m_identifierCheckEdit->text());
+    if (matches.size() > 0) {
+        QUrl url = config.payeeLink(m_identifierCheckEdit->text());
+        m_capturedResult->setText(matches[0]);
+        m_resultingURL->setTextInteractionFlags(Qt::TextBrowserInteraction);
+        m_resultingURL->setText(QString("<a href=\"%1\">%1</a>").arg(url.toString(), url.toString()));
+    }
+}
+
 
 AlkPayeeLinksWidget::AlkPayeeLinksWidget(QWidget *parent)
     : QWidget(parent)
@@ -20,3 +62,5 @@ AlkPayeeLinksWidget::~AlkPayeeLinksWidget()
 {
     delete d;
 }
+
+#include "alkpayeelinkswidget.moc"
