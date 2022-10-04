8f5908b1ec1795f3b33be4b5776101284512a659
Ralf Habacker <ralf.habacker@freenet.de>
2022-10-04 21:01:04 +0200

    Add empty classes AlkPayeeLinks and alkPayeeLinksWidget

---
+++ src/alkpayeelinkswidget.h
@@ -0,0 +1,21 @@
+#ifndef ALKPAYEELINKSWIDGET_H
+#define ALKPAYEELINKSWIDGET_H
+
+#include <alkimia/alk_export.h>
+
+#include <QWidget>
+
+class ALK_EXPORT AlkPayeeLinksWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit AlkPayeeLinksWidget(QWidget *parent = nullptr);
+    ~AlkPayeeLinksWidget();
+
+private:
+    class Private;
+    Private *const d;
+};
+
+#endif // ALKPAYEELINKSWIDGET_H
